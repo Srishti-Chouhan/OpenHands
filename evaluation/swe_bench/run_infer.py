b556ebb4a501e324cf1ebf647f49c4c650cca587
Srishti-Chouhan <srishti.chouhan1100@gmail.com>
2024-11-28 19:09:22 -0500

    changes to eval files

--- evaluation/swe_bench/run_infer.py
+++ evaluation/swe_bench/run_infer.py
@@ -523,6 +523,7 @@ if __name__ == '__main__':
 
     llm_config = None
     if args.llm_config:
+        print(f'Using LLM config: {args.llm_config}')
         llm_config = get_llm_config_arg(args.llm_config)
         llm_config.log_completions = True
 
